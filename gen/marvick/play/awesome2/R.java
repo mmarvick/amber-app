80962ddb831763587fb2de9767a1d17a6c616e50
Michael Marvick <mmarvick@gmail.com>
2013-12-17 22:52:47 -0800

    fixed some small bugs..

--- gen/marvick/play/awesome2/R.java
+++ gen/marvick/play/awesome2/R.java
@@ -25,7 +25,6 @@ public final class R {
         public static final int ic_launcher=0x7f020001;
     }
     public static final class id {
-        public static final int action_settings=0x7f090006;
         public static final int buttonGetCat=0x7f090003;
         public static final int catSelectionList=0x7f090004;
         public static final int cat_descrip=0x7f090002;
@@ -39,20 +38,17 @@ public final class R {
     }
     public static final class menu {
         public static final int main=0x7f080000;
-        public static final int select_cat=0x7f080001;
     }
     public static final class raw {
         public static final int bad_meow=0x7f040000;
         public static final int good_meow=0x7f040001;
     }
     public static final class string {
-        public static final int action_settings=0x7f060009;
         public static final int app_name=0x7f060000;
         public static final int button_get_cat=0x7f060006;
         public static final int cat=0x7f060001;
         public static final int description_bad=0x7f060005;
         public static final int description_good=0x7f060004;
-        public static final int hello_world=0x7f06000a;
         public static final int post_cat=0x7f060003;
         public static final int pre_cat=0x7f060002;
         public static final int selectcat=0x7f060007;
